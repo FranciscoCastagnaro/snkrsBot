e35f75e4184625ea5373faac93005fc2be52d609
Francisco Castagnaro <64395269+FranciscoCastagnaro@users.noreply.github.com>
2025-07-04 03:24:51 -0300

    Update proxys.py

--- proxys.py
+++ proxys.py
@@ -6,8 +6,8 @@ class Proxys:
         self.email = email
         self.password = password
 
-p1 = Proxys("FCastagnaro", 112000000, 44759045, "fran@hotmail.com", "testing")
-p2 = Proxys("NLuque", 112000000, 44670463, "", "")
-p3 = Proxys("SAcevedo", 112000000, 20711288, "san@gmail.com", "testing")
+p1 = Proxys("FCastagnaro", 112000000, 44000000, "fran@hotmail.com", "testing")
+p2 = Proxys("NLuque", 112000000, 44000000, "", "")
+p3 = Proxys("SAcevedo", 112000000, 44000000, "san@gmail.com", "testing")
 
 proxys = [p1, p2, p3] 
